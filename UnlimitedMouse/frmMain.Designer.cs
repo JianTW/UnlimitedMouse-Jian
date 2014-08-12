7f04f8ab06add1ffd5d9b747d118e53b5a019d40
JianTW <motovb@gmail.com>
2014-08-12 15:50:38 +0800

    Modify for supporting that primary screen is at the right side.

---
+++ UnlimitedMouse/frmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace UnlimitedMouse
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tmrMouse = new System.Windows.Forms.Timer(this.components);
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.niMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.miLoopVertically = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrMouse
+            // 
+            this.tmrMouse.Enabled = true;
+            this.tmrMouse.Interval = 300;
+            this.tmrMouse.Tick += new System.EventHandler(this.tmrMouse_Tick);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(24, 32);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 17);
+            this.lblMsg.TabIndex = 0;
+            // 
+            // niMain
+            // 
+            this.niMain.ContextMenuStrip = this.cmsMain;
+            this.niMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niMain.Icon")));
+            this.niMain.Text = "Unlimited Mouse Jian";
+            this.niMain.Visible = true;
+            this.niMain.DoubleClick += new System.EventHandler(this.niMain_DoubleClick);
+            // 
+            // cmsMain
+            // 
+            this.cmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miLoopVertically,
+            this.toolStripSeparator1,
+            this.miExit});
+            this.cmsMain.Name = "cmsMain";
+            this.cmsMain.Size = new System.Drawing.Size(178, 58);
+            // 
+            // miLoopVertically
+            // 
+            this.miLoopVertically.Checked = global::UnlimitedMouse.Properties.Settings.Default.LoopVertically;
+            this.miLoopVertically.CheckOnClick = true;
+            this.miLoopVertically.Name = "miLoopVertically";
+            this.miLoopVertically.Size = new System.Drawing.Size(177, 24);
+            this.miLoopVertically.Text = "Loop &Vertically";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(174, 6);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(177, 24);
+            this.miExit.Text = "&Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 187);
+            this.Controls.Add(this.lblMsg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Unlimited Mouse Jian";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.VisibleChanged += new System.EventHandler(this.frmMain_VisibleChanged);
+            this.cmsMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrMouse;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.NotifyIcon niMain;
+        private System.Windows.Forms.ContextMenuStrip cmsMain;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.ToolStripMenuItem miLoopVertically;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ UnlimitedMouse/frmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UnlimitedMouse
+{
+    public partial class frmMain : Form
+    {
+        private int Threshold = 2;
+        private int TotalWidth = 0;
+        private int TotalHeight = 0;
+
+        //如果滑鼠落在左右邊界的話，就要交換
+        private int LeftThreshold, RightThreshold;
+        public frmMain()
+        {
+            InitializeComponent();
+            //foreach (Screen screen in Screen.AllScreens)
+            //    TotalWidth += screen.Bounds.Width;
+            LeftThreshold = Screen.AllScreens[0].Bounds.X + Threshold;
+            RightThreshold = Screen.AllScreens[Screen.AllScreens.Length - 1].Bounds.Right - Threshold;
+
+            TotalHeight = Screen.PrimaryScreen.Bounds.Height;
+            niMain.Icon = this.Icon;
+            niMain.ShowBalloonTip(2000, "Unlimited Mouse Jian", "Starting...", ToolTipIcon.Info);
+            miLoopVertically.Checked = Properties.Settings.Default.LoopVertically;
+        }
+
+        private void tmrMouse_Tick(object sender, EventArgs e)
+        {
+            Screen[] s = Screen.AllScreens;
+
+            int X = System.Windows.Forms.Cursor.Position.X;
+            int Y = System.Windows.Forms.Cursor.Position.Y;
+            lblMsg.Text = System.Windows.Forms.Cursor.Position.X + ", " + System.Windows.Forms.Cursor.Position.Y;
+
+            if (X < LeftThreshold)
+                System.Windows.Forms.Cursor.Position = new Point(RightThreshold);
+            else if (X > RightThreshold)
+                System.Windows.Forms.Cursor.Position = new Point(LeftThreshold);
+            
+            if (!miLoopVertically.Checked)
+                return;
+
+            if (Y < Threshold)
+                System.Windows.Forms.Cursor.Position = new Point(X, TotalHeight - Threshold);
+            else if (Y > TotalHeight - Threshold)
+                System.Windows.Forms.Cursor.Position = new Point(X, Threshold);
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void frmMain_VisibleChanged(object sender, EventArgs e)
+        {
+#if !DEBUG
+            this.Hide();
+#endif
+        }
+
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.LoopVertically = miLoopVertically.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void niMain_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
